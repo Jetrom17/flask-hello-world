11a140bba0c326dab42ce53ea906af9dd9edc8a1
Jeiel17 <Jeiel17@protonmail.com>
2023-01-26 19:19:41 -0300

    flask

--- api/index.py
+++ api/index.py
@@ -1,11 +1,16 @@
-from flask import Flask
+from flask import Flask, Response
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return 'Hello, World!'
-
-@app.route('/about')
-def about():
-    return 'About'
+@app.route("/")
+def hello_world():
+    return Response("""
+    <style>body {background-color: gold;}</style>
+    <!Docctype html>
+    <html>
+    <center>
+    <p>Hello, World!</p>
+    </center>
+    <div style="position: fixed; left: 0; right: 0; margin: auto; bottom: 0; text-align: center;">Feito em Flask</div>
+    </html>
+    """)
